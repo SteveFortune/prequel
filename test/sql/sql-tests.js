f509a16bd1efefe05a1cbb7ea709e57eb62e364c
Joe Whitfield-Seed <joe.ws@ripjar.com>
2015-08-03 00:11:09 +0100

    Start to migrate pure SQL tests from e2e-tests to sql-tests.
    GROUP BY tests are currently commented out because they need better
    result matching (aggregate column names and non-order-sensitive matching
    of GROUP BY results when there is no ORDER BY).

--- test/sql/sql-tests.js
+++ test/sql/sql-tests.js
@@ -1,4 +1,6 @@
-import test from "tape";
+import tape from "tape";
+import _ from "lodash";
+
 import prequelQuery from "../../src/query";
 import prequelParse from "../../src/parser";
 import createDb, { testData } from "./test-harness";
@@ -6,27 +8,87 @@ import createDb, { testData } from "./test-harness";
 const table = "test";
 const db = createDb(table);
 
-function testQuery(sql, testFunction) {
-  test(sql, function(t) {
+function testQuery(sql, { test=deepEqual, log=false, only=false } = {}) {
+  const tapeFunc = only ? tape.only : tape;
+
+  tapeFunc(sql, function(t) {
     const parsed = prequelParse(sql);
     const prequelResult = prequelQuery(parsed, { [table]: testData });
     const dbResult = db.query(sql);
 
-    if(testFunction) {
-      testFunction(t, prequelResult, dbResult);
-    } else {
-      t.deepEqual(prequelResult, dbResult);
-      t.end();
+    if(log) {
+      t.comment(JSON.stringify(prequelResult));
     }
+
+    test(t, prequelResult, dbResult);
   });
 }
 
+function deepEqual(t, prequelResult, dbResult) {
+  t.deepEqual(prequelResult, dbResult);
+  t.end();
+}
+
+// * Allow the keys COUNT(DISTINCT a) and count_distinct_a to be treated as the same
+// * Some of these cases also need order-independent matching...
+// FIXME
+
+// function matchGroups(t, prequelResults, dbResults) {
+//   const valuesMatch = prequelResults.every((result, i) =>
+//     t.deepEqual(_.values(result), _.values(dbResults[i])));
+//
+//   const keysMatch = prequelResults.every((prequelResult, i) => {
+//     const dbResult = dbResults[i];
+//
+//     return _(dbResult).keys().all(key => {
+//       if(prequelResult[key]) return true;
+//       const prequelKey = key.replace(/\(| /g, "_").slice(0, -1);
+//       return prequelResult[prequelKey];
+//     }).value();
+//   });
+//
+//   t.true(valuesMatch);
+//   t.true(keysMatch);
+//
+//   t.end();
+// }
+
+// SELECT
 testQuery(`SELECT name FROM ${table}`);
+testQuery(`SELECT name, age FROM ${table}`);
+testQuery(`SELECT name, age, greeting AS msg FROM ${table}`);
 testQuery(`SELECT * FROM ${table}`);
-testQuery(`SELECT * FROM ${table} LIMIT 1`);
-testQuery(`SELECT name, greeting AS message, age FROM ${table} LIMIT 10, 100`);
-testQuery(`SELECT COUNT(DISTINCT age) AS ages FROM ${table}`);
+testQuery(`SELECT name AS nom, age FROM ${table}`);
+
+// WHERE
+testQuery(`SELECT name, greeting FROM ${table} WHERE unread > 20`);
+
+// Overall aggregation
+testQuery(`SELECT COUNT(DISTINCT age) AS n_ages FROM ${table}`);
 testQuery(`SELECT AVG(age) AS avg_age FROM ${table}`);
+// testQuery(`SELECT COUNT(DISTINCT age), greeting FROM ${table}`, { test: matchGroups});
+
+// ORDER BY
+testQuery(`SELECT name, age FROM ${table} ORDER BY age`);
+testQuery(`SELECT name, isActive FROM ${table} ORDER BY age ASC`);
+testQuery(`SELECT greeting FROM ${table} ORDER BY greeting DESC`);
+testQuery(`SELECT * FROM ${table} ORDER BY age, company DESC, isActive ASC`);
+
+// GROUP BY
+// testQuery(`SELECT fruit AS food, age FROM ${table} GROUP BY fruit, age`, { test: matchGroups });
+// testQuery(`SELECT company, COUNT(name), MAX(age) FROM ${table} GROUP BY company`, { test: matchGroups });
+// testQuery(`SELECT company, COUNT(age), COUNT(DISTINCT age) FROM ${table} GROUP BY company`, { test: matchGroups });
 testQuery(`SELECT age, COUNT(DISTINCT name) AS names FROM ${table} GROUP BY age ORDER BY age`);
 
-// TODO...
+// LIMIT
+testQuery(`SELECT * FROM ${table} LIMIT 1`);
+testQuery(`SELECT name, greeting AS message, age FROM ${table} LIMIT 10, 100`);
+testQuery(`SELECT * FROM ${table} ORDER BY name DESC LIMIT 4`);
+
+// Mixture
+testQuery(`SELECT age, COUNT(name) AS count, COUNT(DISTINCT company) AS distinct_companies FROM ${table} GROUP BY age ORDER BY age DESC LIMIT 1, 1`);
+
+// TODO:
+// * sorting, grouping by alias
+// * write test function for matching COUNT(DISTINCT a) and count_distinct_a
+// * test for matching with/without strict order (e.h. GROUP BY without ORDER BY)
