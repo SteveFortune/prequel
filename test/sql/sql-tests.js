5c8261466c117a0eada11420d63fe3d8d9c02db6
Joe Whitfield-Seed <joe.ws@ripjar.com>
2015-08-02 22:41:09 +0100

    Add SQL tests. Change boolean -> int in test data.

--- test/sql/sql-tests.js
+++ test/sql/sql-tests.js
@@ -6,16 +6,27 @@ import createDb, { testData } from "./test-harness";
 const table = "test";
 const db = createDb(table);
 
-function testQuery(sql) {
+function testQuery(sql, testFunction) {
   test(sql, function(t) {
     const parsed = prequelParse(sql);
     const prequelResult = prequelQuery(parsed, { [table]: testData });
     const dbResult = db.query(sql);
 
-    t.deepEqual(prequelResult, dbResult);
-    t.end();
+    if(testFunction) {
+      testFunction(t, prequelResult, dbResult);
+    } else {
+      t.deepEqual(prequelResult, dbResult);
+      t.end();
+    }
   });
 }
 
-// TODO write the tests!
 testQuery(`SELECT name FROM ${table}`);
+testQuery(`SELECT * FROM ${table}`);
+testQuery(`SELECT * FROM ${table} LIMIT 1`);
+testQuery(`SELECT name, greeting AS message, age FROM ${table} LIMIT 10, 100`);
+testQuery(`SELECT COUNT(DISTINCT age) AS ages FROM ${table}`);
+testQuery(`SELECT AVG(age) AS avg_age FROM ${table}`);
+testQuery(`SELECT age, COUNT(DISTINCT name) AS names FROM ${table} GROUP BY age ORDER BY age`);
+
+// TODO...
